1b010b8eef9ff303a9e9f45b38df98cde33cbfca
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2021-11-11 10:01:36 +0700

    update.

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ let btn = document.getElementsByTagName('button')[0];
 
 //ketika tombol di klik
 btn.addEventListener('click', function(){
-    tampilkan Halo saya javascript :D
+    //tampilkan Halo saya javascript :D
     foo1.style.display = 'block';
-    tampilkan Salam kenal calon programmer :D dalam 2 detik
+    //tampilkan Salam kenal calon programmer :D dalam 2 detik
     setTimeout(function(){
         foo2.style.display = 'block';
-        dan hilangkan Halo saya javascript :D dalam 2 detik
+       // dan hilangkan Halo saya javascript :D dalam 2 detik
         foo1.style.display = 'none';
     }, 2000);
 });
